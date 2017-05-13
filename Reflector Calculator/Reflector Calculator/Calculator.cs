8e63208a7ef519e83b0f9e1af1c4f98d63c73e7b
yifanxu <yifanxu00@outlook.com>
2017-05-12 23:53:06 -0700

    Calculators

---
+++ Reflector Calculator/Reflector Calculator/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflector_Calculator
+{
+    class NicknameAttribute : Attribute
+    {
+        public string[] names;
+        public NicknameAttribute(params string[] names)
+        {
+            this.names = names;
+        }
+    }
+
+    class Calculator
+    {
+
+        [Nickname("+","plus")]
+        public int add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [Nickname("-","minus")]
+        public int subtract (int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        [Nickname("*","x","times")]
+        public int multiply (int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        [Nickname("/","dividedby")]
+        public int divide (int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                Console.WriteLine("SYSTEM: -Cannot divide by 0");
+                return 0;
+            }
+            return num1 / num2;
+        }
+
+        [Nickname("^", "")]
+        public int power (int basee, int power)
+        {
+            if(basee == 0)
+            {
+                return 0;
+            }
+            if(power == 0)
+            {
+                return 1;
+            }
+            int num = basee;
+            int pow = power;
+            if (power < 0)
+            {
+                pow = -1 * power;
+            }
+            for(int i = 0; i < pow; i++)
+            {
+                num = num * basee;
+            }
+            return num;
+        }
+
+    }
+}
---
+++ Reflector Calculator/Reflector Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflector_Calculator
+{
+    class Program
+    {
+        private static Calculator calculator = new Calculator();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                execute(input);
+            }
+        }
+
+        public static void execute (string input)
+        {
+            string[] parameters = input.Split(' ');
+            if(parameters.Length != 3)
+            {
+                Console.WriteLine("SYSTEM: - Incorrect Input");
+                return;
+            }
+            var methods = calculator.GetType().GetMethods();
+            foreach(var method in methods)
+            {
+                if(IsMethodName(parameters[1],method))
+                {
+                    int num1 = 0;
+                    int num2 = 0;
+                    if(!int.TryParse(parameters[0], out num1) || !int.TryParse(parameters[2],out num2))
+                    {
+                        Console.WriteLine("SYSTEM: - Invalid Numbers");
+                    }
+                    Console.WriteLine("SYSTEM: -Result is {0}", method.Invoke(calculator, new object[] { num1, num2 }));
+                    return;
+                }
+            }
+            Console.WriteLine("SYSTEM: - Invalid Operation");
+        }
+
+        public static bool IsMethodName(string methodName, System.Reflection.MethodInfo method)
+        {
+            if (String.Equals(methodName, method.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            NicknameAttribute attibute = (NicknameAttribute)method.GetCustomAttribute(typeof(NicknameAttribute));
+            foreach (string nickname in attibute.names)
+            {
+                if (String.Equals(methodName,nickname,StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

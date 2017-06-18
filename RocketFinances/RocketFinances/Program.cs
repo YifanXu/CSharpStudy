e85ee5e8a3480d92d946d81a5e7b2bc5ac83af47
yifan <yifanxu00@outlook.com>
2017-06-17 18:10:15 -0700

    Recursion HW

---
+++ RocketFinances/RocketFinances/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RocketFinances
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+		}
+
+		public static int GetTotalCost (Task rootTask){
+			const int cost = 10000;
+
+			List<Task> allTasks = GetAllTasks (rootTask);
+			//Count up the total
+			int totalCost = 0;
+			foreach (Task task in allTasks) {
+				totalCost += task.time*cost;
+			}
+			return totalCost;
+		}
+
+		public static List<Task> GetAllTasks (Task finalTask){
+			List<Task> countedTasks = new List<Task> ();
+			List<Task> layerMember = new List<Task> ();
+			List<Task> newLayerMember = new List<Task> { finalTask };
+			while (newLayerMember.Count != 0) {
+				layerMember.Clear ();
+				layerMember.AddRange (newLayerMember);
+				newLayerMember.Clear ();
+
+				foreach (Task member in layerMember) {
+					//Checking if the memeber is already counted
+					bool memeberAlreadyExists = false;
+					foreach (Task countedTask in countedTasks) {
+						if(object.ReferenceEquals(countedTask,member)){
+							memeberAlreadyExists = true;
+							break;
+						}
+					}
+					if (memeberAlreadyExists) {
+						continue;
+					}
+					newLayerMember.AddRange (member.dependentTasks);
+					countedTasks.Add (member);
+				}
+			}
+
+			return countedTasks;
+		}
+
+		public static int GetShortestTime(Task rootTask){
+			
+		}
+	}
+}
---
+++ RocketFinances/RocketFinances/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RocketFinances
+{
+	public class Task
+	{
+		public int time;
+		public Task[] dependentTasks;
+
+		public Task(){
+		}
+
+		public Task (int time, Task[] dependency)
+		{
+			this.dependentTasks = dependency;
+			this.time = time;
+		}
+
+		public int maxRequiredTime {
+			get{
+				int longestDependentTime = 0;
+				for (int i = 0; i < dependentTasks.Length; i++) {
+					if (dependentTasks [i].maxRequiredTime > longestDependentTime) {
+						longestDependentTime = dependentTasks [i].maxRequiredTime;
+					}
+				}
+				return this.time + longestDependentTime;
+			}
+		}
+	}
+}
+
